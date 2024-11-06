e21efa6030fbed9bcffa6c757f309ad7cf6d5221
hirokiasano1 <hirokiasanovieira@gmail.com>
2024-11-06 00:49:31 -0300

    mysqlteste

--- src/main/java/com/Cocacola/pricemanagementapi/Repository/LevelRepository.java
+++ src/main/java/com/Cocacola/pricemanagementapi/Repository/LevelRepository.java
@@ -8,12 +8,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LevelRepository extends JpaRepository<Level, Long> {
-
-    @Modifying
-    @Transactional
+     @Modifying
     @Query(
-            value = "ALTER SEQUENCE tb_level_id_seq RESTART WITH 1",
+            value = "ALTER TABLE tb_level AUTO_INCREMENT = 1",
             nativeQuery = true
     )
     void resetSequence();
+    
 }
